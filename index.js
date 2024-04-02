c8e318aeb5c258cb703d676e1d9d24941d4c6d43
Luther Isaboke <lusaboke@gmail.com>
2024-04-02 11:17:08 +0300

    arrwfuncs

---
+++ index.js
@@ -0,0 +1,11 @@
+//implementing a function expression called divide
+const divide = (a,b) => a/b;
+divide(200/100);
+
+//implementing an arrow function called square
+const square = (x) => x**2;
+square(2);
+ 
+//implementing a function called add
+const add = (a,b) => a+b;
+add(3,4);
